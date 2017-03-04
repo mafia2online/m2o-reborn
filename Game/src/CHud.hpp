f36e97625f62de66d00632a5aa347a0e3aa7b766
Tyldar <darreugne@hotmail.fr>
2017-03-04 19:03:09 +0100

    Fixed FaderFadeOut

--- Game/src/CHud.hpp
+++ Game/src/CHud.hpp
@@ -1,33 +1,6 @@
 #pragma once
 #include "CommonHeaders.h"
 
-/*
-#include "CHud.h"
-
-using namespace M2;
-
-#define C_Hud_Ptr 0x1CBA618
-
-C_Hud::C_Hud() : m_interface(*(ICHud**)C_Hud_Ptr)
-{
-
-}
-
-void M2::C_Hud::FaderFadeIn(float time)
-{
-static uint32_t dwReturn;
-float test;
-Mem::InvokeFunction<Mem::call_this, void>(0x08910C0, &m_interface->m_pEnumerator, &dwReturn, &test, time, 0, 0);
-}
-
-void M2::C_Hud::FaderFadeOut(float time)
-{
-static uint32_t dwReturn;
-float test;
-Mem::InvokeFunction<Mem::call_this, void>(0x0890F10, &m_interface->m_pEnumerator, &dwReturn, &test, time, 0, 0);
-}
-*/
-
 namespace M2
 {
 	class ICGameGuiModule
@@ -39,24 +12,17 @@ namespace M2
 	class C_GameGuiModule : public GameClassWrapperStatic<C_GameGuiModule, 0x1AB64F0>
 	{
 	public:
-		/*void _declspec(naked) FaderFadeIn(int un)
-		{
-
-		}*/
-
 		void FaderFadeIn(float time)
 		{
 			static uint32_t dwReturn;
 			Mem::InvokeFunction<Mem::call_this, void>(0x472300, this, &dwReturn, time, 0);
 		}
-		/*
+
 		void FaderFadeOut(float time)
 		{
 			static uint32_t dwReturn;
-			float test;
-			Mem::InvokeFunction<Mem::call_this, void>(0x0890F10, this, &dwReturn, &test, time, 0, 0);
+			Mem::InvokeFunction<Mem::call_this, void>(0x472330, this, &dwReturn, time, 0);
 		}
-		*/
 
 		inline ICGameGuiModule* GetInterface() { return reinterpret_cast<ICGameGuiModule*>(this); }
 	};
