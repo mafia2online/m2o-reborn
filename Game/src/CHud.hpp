689c3b0bfa3b887068bd52a3814c0337514d248a
Tyldar <darreugne@hotmail.fr>
2017-04-17 23:24:45 +0200

    Reversed CTimer

--- Game/src/CHud.hpp
+++ Game/src/CHud.hpp
@@ -8,19 +8,23 @@
 #pragma once
 #include "CommonHeaders.h"
 #include "CHudScriptW.h"
+#include "CTimer.hpp"
 
 namespace M2
 {
 	class ICHud
 	{
 	public:
-		pad(ICHud, pad0, 0xA0);
+		pad(ICHud, pad0, 0x6C);			//0000 - 006C
+		C_Timer *m_pTimer;				//006C - 0070
+		pad(ICHud, pad1, 0x30);			//0070 - 00A0
 		C_HudScriptW *m_pScript;
 	};
 
 	class C_Hud : public GameClassWrapper<C_Hud, ICHud, 0x1CBA618>
 	{
 	public:
+		C_Timer	*GetHudTimer() { return reinterpret_cast<ICHud *>(this)->m_pTimer; }
 		C_HudScriptW	*GetHudScript() { return reinterpret_cast<ICHud *>(this)->m_pScript; }
 	};
 };
