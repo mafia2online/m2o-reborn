6cc7e5488a28f418c51350cf28e0566126e1dc9d
Tyldar <darreugne@hotmail.fr>
2017-05-28 15:59:57 +0200

    Added C_HumanInventory methods

--- Game/src/CHumanInventory.hpp
+++ Game/src/CHumanInventory.hpp
@@ -18,6 +18,11 @@ namespace M2
 	class C_HumanInventory : public ICHumanInventory
 	{
 	public:
+		void AddAmmo(int weapon, int ammo)
+		{
+			Mem::InvokeFunction<Mem::call_this, int>(0x0942B00, this, weapon, ammo);
+		}
+
 		void AddMoney(int amount, int unk)
 		{
 			Mem::InvokeFunction<Mem::call_this, int>(0x095B060, this, amount, unk);
@@ -26,6 +31,56 @@ namespace M2
 		void AddWeapon(int weapon, int ammo)
 		{
 			Mem::InvokeFunction<Mem::call_this, bool>(0x0956BF0, this, weapon, ammo);
+		}
+
+		bool CanAddAmmo(int weapon, int ammo)
+		{
+			return Mem::InvokeFunction<Mem::call_this, int>(0x0942AE0, this, weapon, ammo);
+		}
+
+		bool CanAddWeapon(int weapon)
+		{
+			return Mem::InvokeFunction<Mem::call_this, int>(0x0942AB0, this, weapon);
+		}
+
+		bool CanFire()
+		{
+			return Mem::InvokeFunction<Mem::call_this, bool>(0x0420230, this);
+		}
+
+		bool CanReload()
+		{
+			return Mem::InvokeFunction<Mem::call_this, bool>(0x0420510, this);
+		}
+
+		void DoDrop()
+		{
+			Mem::InvokeFunction<Mem::call_this, bool>(0x0989F00, this);
+		}
+
+		void DoReload(int unk, int unk2)
+		{
+			Mem::InvokeFunction<Mem::call_this, bool>(0x0969510, this, unk, unk2);
+		}
+
+		void DoShot(int unk)
+		{
+			Mem::InvokeFunction<Mem::call_this, bool>(0x09898B0, this, unk);
+		}
+
+		int GetReloadAmount(int unk, int unk2)
+		{
+			return Mem::InvokeFunction<Mem::call_this, int>(0x09693F0, this, unk, unk2);
+		}
+
+		void Reset()
+		{
+			Mem::InvokeFunction<Mem::call_this, bool>(0x098CFB0, this);
+		}
+
+		int TellAmmoInSelectedWeapon()
+		{
+			return Mem::InvokeFunction<Mem::call_this, int>(0x0942D80, this);
 		}
 	};
 };
