d6372871a9bf1de75ed027b8321c9eafe75ae8f2
Tyldar <darreugne@hotmail.fr>
2017-05-30 21:38:15 +0200

    Reversed C_MafiaFramework

--- Game/src/CGameGuiModule.hpp
+++ Game/src/CGameGuiModule.hpp
@@ -29,5 +29,15 @@ namespace M2
 			static uint32_t dwReturn;
 			Mem::InvokeFunction<Mem::call_this, void>(0x472330, this, &dwReturn, time, 0);
 		}
+
+		bool IsMainMenuOpened()
+		{
+			return Mem::InvokeFunction<Mem::call_cdecl, bool>(0x048C9C0);
+		}
+
+		void OpenPauseMenuOverIngame()
+		{
+			Mem::InvokeFunction<Mem::call_cdecl, int>(0x04E0540);
+		}
 	};
 };
