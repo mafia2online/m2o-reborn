382e8037664a76892163238e7d8cde124a123ae9
Tyldar <darreugne@hotmail.fr>
2017-07-23 20:02:52 +0200

    Added server console command processing

--- Server/src/CCore.cpp
+++ Server/src/CCore.cpp
@@ -1,6 +1,6 @@
 #include "CCore.h"
 
-CCore::CCore() : m_debuglog(true, true)
+CCore::CCore() : m_debuglog(true, true), m_readInput()
 {
 
 }
@@ -25,7 +25,61 @@ bool CCore::Init()
 	printf("Server Port = %d\n", m_serverSettings.GetServerPort());
 	printf("Server Password = %s\n", m_serverSettings.GetServerPassword().c_str());
 
-	while (true) {
+	m_active = true;
+	m_readInput = true;
+	m_inputThread = std::thread(&CCore::ReadInput, this);
+
+	while (m_active) {
 		m_networkManager.Pulse();
+		HandleInput();
+	}
+}
+
+void CCore::Stop()
+{
+	m_active = false;
+	m_readInput = false;
+	m_inputThread.join();
+}
+
+void CCore::ReadInput()
+{
+	std::string	input;
+	while (m_readInput)
+	{
+		std::getline(std::cin, input);
+		m_inputMutex.lock();
+		m_inputQueue.push(input);
+		m_inputMutex.unlock();
+	}
+}
+
+void CCore::ProcessInput(const std::string& command, const std::string& params)
+{
+	if (command == "exit" || command == "quit" || command == "stop") {
+		m_active = false;
+	}
+}
+
+void CCore::HandleInput()
+{
+	if (m_inputMutex.try_lock())
+	{
+		while (!m_inputQueue.empty())
+		{
+			std::string	input = m_inputQueue.back();
+			std::size_t	paramsOffset = input.find(" ") + 1;
+			std::string	command = input.substr(0, paramsOffset - 1);
+			std::string params = input.substr(paramsOffset);
+
+			if (input.empty())
+			{
+				m_inputMutex.unlock();
+				return;
+			}
+			ProcessInput(command, params);
+			m_inputQueue.pop();
+		}
+		m_inputMutex.unlock();
 	}
 }
