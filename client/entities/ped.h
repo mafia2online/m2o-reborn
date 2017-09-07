eefd09b6640e32abcca2fa6e3b0728888e59ce60
Tyldar <darreugne@hotmail.fr>
2017-09-07 18:23:38 +0200

    Edited ped creation

--- client/entities/ped.h
+++ client/entities/ped.h
@@ -2,38 +2,54 @@ void ped_oncreate(librg_event_t *event)
 {
     auto transform = librg_fetch_transform(event->entity);
 
-    mod_log("creating player\n");
+    const char *directory = "/sds/traffic/";
+    const char *model = "cvezjon";
 
-    M2::C_Human2 *ent = M2::C_EntityFactory::Get()->CreateEntity<M2::C_Human2>(M2::EntityTypes::Entity_Human);
+    M2::Wrappers::ModelManager *pModelManager = new M2::Wrappers::ModelManager();
+    if (!pModelManager) {
+        return;
+    }
 
-    librg_assert_msg(ent, "player entity should be created!");
+    M2::Wrappers::GameModelManager *pPedModelManager = pModelManager->Load(directory, model);
+    if (!pPedModelManager) {
+        return;
+    }
 
-    M2::C_Model *own_model = M2::C_Core::Get()->AllocateModel(2);
-    own_model->CloneHierarchy(M2::C_PlayerModelManager::Get()->GetInterface()->m_pModel);
+    M2::C_Human2 *human = M2::C_EntityFactory::Get()->CreateEntity<M2::C_Human2>(M2::EntityTypes::Entity_Human);
+    if (!human) {
+        return;
+    }
 
-    own_model->SetName("lawl");
-    own_model->MarkForNotify(2);
+    M2::C_Model *pModel = M2::C_Core::Get()->AllocateModel(2);
+    if (!pModel) {
+        return;
+    }
 
-    reinterpret_cast<M2::C_Entity *>(ent)->SetModel(own_model);
-    reinterpret_cast<M2::C_Entity *>(ent)->Setup();
+    pModel->CloneHierarchy(pPedModelManager->GetModelManager()->m_pModel);
+    if (!pModel) {
+        return;
+    }
 
-    // set flagsF
-    DWORD flags = *(DWORD *)(ent + 32) & 0xFFFFFBF | 0x4800;
-    *(DWORD *)(ent + 32) = flags;
+    pModel->SetName("m2online_ped");
+    pModel->MarkForNotify(2);
+
+    reinterpret_cast<M2::C_Entity *>(human)->SetModel(pModel);
+    reinterpret_cast<M2::C_Entity *>(human)->Setup();
+
+    DWORD flags = *(DWORD *)(human + 32) & 0xFFFFFBF | 0x4800;
+    *(DWORD *)(human + 32) = flags;
 
     if (flags & 0x20)
         mod_log("Flags set sucessfully!\n");
 
-    reinterpret_cast<M2::C_Entity *>(ent)->Activate();
+    reinterpret_cast<M2::C_Entity *>(human)->Activate();
 
-    if (reinterpret_cast<M2::C_Entity *>(ent)->IsActive())
+    if (reinterpret_cast<M2::C_Entity *>(human)->IsActive())
         mod_log("Entity active !\n");
 
-    reinterpret_cast<M2::C_Entity *>(ent)->SetPosition(transform->position);
+    reinterpret_cast<M2::C_Entity *>(human)->SetPosition(transform->position);
 
-    librg_attach_gamedata(event->entity, { (M2::C_Entity*)ent });
-
-    mod_log("Created at %x!\n", ent);
+    mod_log("Created at %x!\n", human);
 }
 
 void ped_onclient(librg_event_t *event)
