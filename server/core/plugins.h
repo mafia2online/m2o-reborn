36d43e7ec19ec185ab5d484bac409653c5d66d30
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2018-03-04 18:10:17 +0200

    added proper server plugin support

--- server/core/plugins.h
+++ server/core/plugins.h
@@ -1,42 +1,32 @@
+#define M2O_PLUGIN_DIRECTORY "plugins"
 typedef void (m2o_plugin_init)(m2o_api_vtable *);
 
 void m2o_plugins_init(m2o_api_vtable *api) {
-    // TODO: refactor to proper platform methods
-    // doing some wierd system command stuff for now :p
+    // create folder if doesnt exist
+    zpl_path_mkdir(M2O_PLUGIN_DIRECTORY, 0744);
 
-    char buffer[1024];
-    // zpl_system_command("mkdir -p plugins", buffer);
-    zpl_zero_item(buffer);
+    zpl_string_t filelst = {0};
+    filelst = zpl_path_dirlist(zpl_heap(), M2O_PLUGIN_DIRECTORY, false);
+    char **files = zpl_str_split_lines(zpl_heap(), filelst, false);
 
-    char filename[128];
+    for (i32 i = 0; i < zpl_array_count(files); ++i) {
+        zpl_dll_handle dll = zpl_dll_load(files[i]);
 
-    // WARNING: ugly code below
-    // TODO: refactor with proper string and platform methods
-    // for (int i = 0, j = 0; i < zpl_strlen(buffer); ++i, ++j) {
-    //     if (buffer[i] == '\n') {
-    //         filename[j] = '\0';
-    //         // mod_log("[info] trying to load: %s\n", filename);
+        if (!dll) {
+            mod_log("[error] could not load a plugin: %s\n", files[i]);
+        } else {
+            auto plugin_main = (m2o_plugin_init *)zpl_dll_proc_address(dll, "m2o_plugin_main");
 
-    //         char filepath[256];
-    //         zpl_snprintf(filepath, 256, "plugins/%s", filename);
-    //         zpl_dll_handle_t dll = zpl_dll_load(filepath);
+            if (!plugin_main) {
+                mod_log("[error] could not load a plugin: %s\n", files[i]);
+            } else {
+                mod_log("[info] successfully loaded plugin: %s\n", files[i]);
+                plugin_main(api);
+            }
+        }
 
-    //         if (!dll) {
-    //             mod_log("[error] could not load a plugin: %s\n", filename);
-    //         } else {
-    //             auto plugin_main = (m2o_plugin_init *)zpl_dll_proc_address(dll, "m2o_plugin_main");
+    }
 
-    //             if (!plugin_main) {
-    //                 mod_log("[error] could not load a plugin: %s\n", filename);
-    //             } else {
-    //                 mod_log("[info] successfully loaded plugin: %s\n", filename);
-    //                 plugin_main(api);
-    //             }
-    //         }
-
-    //         zpl_zero_item(filename); j = -1;
-    //     } else {
-    //         filename[j] = buffer[i];
-    //     }
-    // }
+    zpl_string_free(filelst);
+    zpl_array_free(files);
 }
