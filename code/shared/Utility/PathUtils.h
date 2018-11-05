10b23afbf18af03e4cc5d7a05653716048f844f7
Vladyslav Hrytsenko <vladgritsenko@gmail.com>
2018-11-05 16:51:35 +0200

    removed invalid names

--- code/shared/Utility/PathUtils.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-namespace Utility
-{
-    std::wstring MakeAbsolutePathW(const std::wstring &rel_to);
-    std::string MakeAbsolutePathA(const std::string &rel_to);
-}
