60181c26e0b64656e520d7f256ff945a64ba6cf9
Martin Kammersberger <Martinjk@outlook.com>
2018-11-06 13:28:31 +0100

    Fix for game crash on exit

--- code/client/core/Core/MartinDevelopment.cpp
+++ code/client/core/Core/MartinDevelopment.cpp
@@ -8,7 +8,10 @@
 // be lovely and just watch, do not touch
 // or I will touch you!
 
-static nomad::base_function init([]()
-{
-    
+static nomad::base_function init([]() {
+
+    // Fix for game shutdown function stack (esp) cleanup destroying the stack
+    // causing the game to crash (we need this since we're jumping inside the game
+    // startup without doing the stack preparations at the start of WinMain).
+    nio::nop(0x00401D71, 10);
 });
